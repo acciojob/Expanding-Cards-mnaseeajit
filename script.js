be23794d9b65634b0c4dfb7809702800926d6789
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-22 13:31:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+    const panels = document.querySelectorAll('.panel');
+
+    panels.forEach(panel => {
+        panel.addEventListener('click', function () {
+            removeActiveClasses();
+            this.classList.add('active');
+        });
+    });
+
+    function removeActiveClasses() {
+        panels.forEach(panel => {
+            panel.classList.remove('active');
+        });
+    }
+});
